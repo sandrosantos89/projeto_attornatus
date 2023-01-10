31b9e9b6f97318db800dff73525fbaf9c6c1590f
Sandro Santos <sandro.ads.dev@gmail.com>
2023-01-10 18:58:12 -0300

    Primeiro commit

---
+++ src/main/java/com/ssantos/api/attornatus/ProjetoAttornatusApplication.java
@@ -0,0 +1,13 @@
+package com.ssantos.api.attornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAttornatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssantos/api/attornatus/ProjetoAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssantos.api.attornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
