ffefee344735388b4bd2a20d57d9b7ae5b15fb39
Sandro Santos <sandro.ads.dev@gmail.com>
2023-01-11 21:09:59 -0300

    Get, Put, Delete, Update Usuario

--- src/main/java/com/ssantos/api/attornatus/resources/UsuarioResource.java
+++ src/main/java/com/ssantos/api/attornatus/resources/UsuarioResource.java
@@ -1,11 +1,19 @@
 package com.ssantos.api.attornatus.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ssantos.api.attornatus.domain.Usuario;
 import com.ssantos.api.attornatus.services.UsuarioService;
@@ -17,10 +25,36 @@ public class UsuarioResource {
 	@Autowired
 	private UsuarioService service;
 
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll() {
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
 		Usuario obj = this.service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj) {
+		Usuario newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> create(@RequestBody Usuario obj) {
+		Usuario newObj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
 	}
 
 }
--- src/main/java/com/ssantos/api/attornatus/services/UsuarioService.java
+++ src/main/java/com/ssantos/api/attornatus/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.ssantos.api.attornatus.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,26 @@ public class UsuarioService {
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado ID: " + id + ", Tipo: " + Usuario.class.getName()));
+	}
+
+	public List<Usuario> findAll() {
+		return repository.findAll();
+	}
+
+	public Usuario update(Long id, Usuario obj) {
+		Usuario newObj = findById(id);
+		newObj.setNome(obj.getNome());
+		newObj.setDataNascimento(obj.getDataNascimento());
+		return repository.save(newObj);
+	}
+
+	public Usuario create(Usuario obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		findById(id);
+		repository.deleteById(id);
 	};
 }
